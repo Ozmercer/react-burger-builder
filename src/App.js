e05e2c619cda2aa87e87775266dd8cf135df4b8f
Ozmrcr <ozmercer@gmail.com>
2020-06-07 17:01:07 +0100

    convert app to functional component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component, Suspense } from 'react';
+import React, { Component, Suspense, useEffect } from 'react';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
@@ -11,45 +11,43 @@ const Checkout = React.lazy(() => import('./containers/Checkout/Checkout'));
 const Orders = React.lazy(() => import('./containers/Orderes/Orders'));
 const Logout = React.lazy(() => import('./containers/Auth/Logout/Logout'));
 
-class App extends Component {
-  componentDidMount() {
-    this.props.onTryAutoSignin();
-  }
+const app = props => {
+  useEffect(() => {
+    props.onTryAutoSignin();
+  }, []);
 
-  render() {
-    let routes = (
+  let routes = (
+    <Switch>
+      <Route path="/build" component={BurgerBuilder} />
+      <Route path="/login" component={Auth} />
+      <Redirect from='/' to="build" />
+    </Switch>
+  );
+
+  if (props.isAuthenticated) {
+    routes = (
       <Switch>
         <Route path="/build" component={BurgerBuilder} />
+        <Route path="/checkout" component={Checkout} />
+        <Route path="/orders" component={Orders} />
         <Route path="/login" component={Auth} />
+        <Route path="/logout" component={Logout} />
         <Redirect from='/' to="build" />
       </Switch>
-    );
-
-    if (this.props.isAuthenticated) {
-      routes = (
-        <Switch>
-          <Route path="/build" component={BurgerBuilder} />
-          <Route path="/checkout" component={Checkout} />
-          <Route path="/orders" component={Orders} />
-          <Route path="/login" component={Auth} />
-          <Route path="/logout" component={Logout} />
-          <Redirect from='/' to="build" />
-        </Switch>
-      )
-    }
-
-    return (
-      <BrowserRouter>
-        <div>
-          <Layout>
-            <Suspense fallback={Spinner}>
-              {routes}
-            </Suspense>
-          </Layout>
-        </div>
-      </BrowserRouter>
-    );
+    )
   }
+
+  return (
+    <BrowserRouter>
+      <div>
+        <Layout>
+          <Suspense fallback={Spinner}>
+            {routes}
+          </Suspense>
+        </Layout>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 const mapStateToProps = state => {
@@ -64,4 +62,4 @@ const mapDispatchToProps = dispatch => {
   }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(app);
